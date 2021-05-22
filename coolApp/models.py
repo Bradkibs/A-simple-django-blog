bcc56d2e13effc85d33f72642e017419526a4c73
Bradley Kibwana <Bradleykibs@gmail.com>
2021-05-22 10:36:43 +0300

    my first django project of a blog.

---
+++ coolApp/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+# Create your models here.
+class Post(models.Model):
+    #STATUS_CHOICES = (('draft', 'Draft'),('published', 'Published'),)
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #slug = models.SlugField(max_length=50, unique_for_date='date_posted')
+    #publish = models.DateTimeField(default=timezone.now)
+    #created = models.DateTimeField(auto_now_add=True)
+    #updated = models.DateTimeField(auto_now=True)
+    #status = models.CharField(max_length=10,
+    #                          choices=STATUS_CHOICES,
+    #                          default='draft')
+
+    def __str__(self):
+        return self.title
+    #def  get_absolute_url(self):
+        #return reverse('post-detail' , kwargs={'pk':self.pk})   
+    
